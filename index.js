f893a20fd34c1967dce34f2d810bc291740096f0
Elvin <elvin.uchendu2020@gmail.com>
2022-12-18 14:43:12 -0800

    added page controller js file  to eventually scraping scripts

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const browserObject = require('./browser');
+const scraperController = require('./pageController');
+
+//Start the browser and create a browser instance
+let browserInstance = browserObject.startBrowser();
+
+// Pass the browser instance to the scraper controller
+scraperController(browserInstance)
+
+
+
 
 //get request to walgreens
 axios
