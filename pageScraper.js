7a68b576f1f347d17476c468b5560942b5d2e222
Elvin <elvin.uchendu2020@gmail.com>
2022-12-18 22:24:19 -0800

    fixed spelling error in comments

--- pageScraper.js
+++ pageScraper.js
@@ -6,7 +6,7 @@ const scraperObject = {
 		await page.goto(this.url);
 		// Wait for the required DOM to be rendered
 		await page.waitForSelector('.CA__Featured-categories__full-width');
-		// Get the link to all the required links i
+		// Get the link to all the required links in the featured categories
 		let urls = await page.$$eval('.list__contain >  ul#at-hp-rp-featured-ul > li', links => {
 			// Extract the links from the data
 			links = links.map(el => el.querySelector('li > a').href)
